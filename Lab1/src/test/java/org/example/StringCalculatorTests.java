d8c0999aef333d8064c77e8c681951476120fd32
Yevgen Shkinder <jekashkinder@gmail.com>
2023-09-25 00:12:58 +0300

    StringCalculator(task1)

---
+++ Lab1/src/test/java/org/example/StringCalculatorTests.java
@@ -0,0 +1,26 @@
+package org.example;
+
+//import org.junit.*
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTests {
+
+    private final StringCalculator StrCalc = new StringCalculator();
+
+    @Test
+    public void shouldCalculateEmpty(){
+        Assertions.assertEquals(0,StrCalc.add(""));
+    }
+
+    @Test
+    public void shouldCalculateOne(){
+        Assertions.assertEquals(1,StrCalc.add("1"));
+    }
+
+    @Test
+    public void shouldCalculateTwo(){
+        Assertions.assertEquals(3,StrCalc.add("1,2"));
+    }
+
+}
