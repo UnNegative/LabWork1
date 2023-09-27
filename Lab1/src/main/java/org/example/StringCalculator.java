a79fc8ecfc7a4a35c222bdf7ebceaf34108470b1
Yevgen Shkinder <jekashkinder@gmail.com>
2023-09-27 05:04:15 +0300

    StringCalculator(Step3)

--- Lab1/src/main/java/org/example/StringCalculator.java
+++ Lab1/src/main/java/org/example/StringCalculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 //import com.sun.jdi.IntegerValue;
 
-import java.security.InvalidParameterException;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
@@ -10,20 +10,41 @@ public class StringCalculator {
     private int result = 0;
     public StringCalculator(){};
 
-    int add(String numStr)/* throws Exception*/ {
+    String[] mySplit(String numStr,String Delimiter) throws Exception{ //throws Exception{
+
+        if(CheckForDelimiterAtTheEnd(numStr,Delimiter)) {
+        throw new Exception();
+        }
+        return numStr.split("["+Delimiter+"]",0);
+    }
+
+
+    boolean CheckForDelimiterAtTheEnd (String numStr, String Delimiter){
+        String str = ("[" + Delimiter + "]$").toString();
+        return Pattern.compile(str).matcher(numStr).find();
+    }
+
+    public int add (String numStr) {
+        try{
+        String[] StringMas = mySplit(numStr,",\n");
+
         if (numStr.isEmpty()) {
             return result;
         }
-        if (numStr.split(",").length == 1) {
+        if (StringMas.length == 1) {
             return Integer.parseInt(numStr);
         }
-        if (numStr.split(",").length >= 2) {
-            for (String num : numStr.split(",")) {
+        if (StringMas.length >= 2) {
+            for (String num : StringMas) {
                 result += Integer.parseInt(num);
             }
             return result;
         }
             return 0;
+        }catch (Exception e){
+            return -1;}
     }
-    }
+}
+
+
 
