58398eccae979c9bcaed797e93cdc8b2f70ff476
Yevgen Shkinder <jekashkinder@gmail.com>
2023-10-11 22:07:35 +0300

    StringCalculator step 2

--- RemasterLab1/src/main/java/org/example/StringCalculator.java
+++ RemasterLab1/src/main/java/org/example/StringCalculator.java
@@ -3,26 +3,29 @@ package org.example;
 public class StringCalculator {
 
     //    public String numStr;
-    private int result = 0;
+//    private int result = 0;
     public StringCalculator(){};
 
     int add(String numStr) throws StringCalculatorInvalidInputException {
+
+        int result = 0;
+
         if (numStr.isEmpty()) {
             return result;
         }
         if (numStr.split(",").length == 1) {
             return Integer.parseInt(numStr);
         }
-        if (numStr.split(",").length == 2) {
+        if (numStr.split(",").length >= 2) {
             for (String num : numStr.split(",")) {
                 result += Integer.parseInt(num);
             }
             return result;
         }
-        if (numStr.split(",").length >2) {
-//            System.out.println("ERROR More than 2 numbers inserted");
-            throw new StringCalculatorInvalidInputException("ERROR More than 2 numbers inserted");
-        }
+//        if (numStr.split(",").length >2) {
+////            System.out.println("ERROR More than 2 numbers inserted");
+//            throw new StringCalculatorInvalidInputException("ERROR More than 2 numbers inserted");
+//        }
         return result;
     }
 }
