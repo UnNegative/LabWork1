98a132ceae6157f987c7792d47d9bbd1d5744a43
Yevgen Shkinder <jekashkinder@gmail.com>
2023-10-13 10:49:10 +0300

    deletions

--- RemasterLab1/src/main/java/org/example/StringCalculatorInvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class StringCalculatorInvalidInputException extends IllegalArgumentException{
-
-    public StringCalculatorInvalidInputException(String message){
-        super(message);
-    }
-}
--- RemasterLab1/src/main/java/org/example/StringCalculatorNegativeException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class StringCalculatorNegativeException extends IllegalArgumentException{
-    public StringCalculatorNegativeException(String message){
-        super(message);
-    }
-}
