94ff65d46f428c656631d31017c4367a6810922a
Yevgen Shkinder <jekashkinder@gmail.com>
2023-11-10 00:08:19 +0300

    step2 Constructors

--- Lab2/src/main/java/org/example/Matrix.java
+++ Lab2/src/main/java/org/example/Matrix.java
@@ -6,14 +6,51 @@ public class Matrix {
 
     private int rows;
     private int columns;
+    private double[][] content;
 
-    public Matrix(){}
+    //Empty matrix
+    public Matrix(){
+        setRows(0);
+        setColumns(0);
+        createMatrix(0,0);
+    }
 
+    public Matrix(int row,int column) throws NegativeMatrixSizeException{
+        if (row<0 || column<0)
+        {
+            throw new NegativeMatrixSizeException("Negative number of rows or columns!");
+        }
+        setRows(row);
+        setColumns(column);
+        createMatrix(row,column);
+    }
+
+    public Matrix(Matrix myMatrice){
+        setRows(myMatrice.rows);
+        setColumns(myMatrice.columns);
+        copyMatrix(myMatrice);
+    }
+
+
+    void createMatrix(int row,int column){
+        content = new double[row][column];
+    }
+
+    void copyMatrix(Matrix matrice){
+        content = new double[matrice.rows][matrice.columns];
+    }
     public void setRows(int rows) {
         this.rows = rows;
     }
 
     public void setColumns(int columns) {
         this.columns = columns;
+    }
+
+    int getRows(){
+        return this.rows;
+    }
+    int getColumns(){
+        return this.columns;
     }
 }
--- Lab2/src/test/java/org/example/LabTests.java
+++ Lab2/src/test/java/org/example/LabTests.java
@@ -1,5 +1,4 @@
 package org.example;
-import org.example.Matrix;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,6 +7,29 @@ import org.junit.jupiter.api.Test;
 public class LabTests {
     @Test
     void EmptyMatriceCreate(){
-
+        Matrix A = new Matrix();
+        Assertions.assertEquals(A.getRows(),0);
+        Assertions.assertEquals(A.getColumns(),0);
     }
+    @Test
+    void RowByColumnMatriceCreate() throws NegativeMatrixSizeException {
+        Matrix A = new Matrix(4,4);
+        Assertions.assertEquals(A.getRows(),4);
+        Assertions.assertEquals(A.getColumns(),4);
+    }
+    @Test
+    void NegativeDimensinOfMatrice() throws NegativeMatrixSizeException {
+//        Matrix A = new Matrix(-3,-1);
+        Assertions.assertThrows(NegativeMatrixSizeException.class,()->new Matrix(-3,-1));
+        Assertions.assertThrows(NegativeMatrixSizeException.class,()->new Matrix(-3,-1));
+    }
+
+    @Test
+    void CopyingMatricesDimension() throws NegativeMatrixSizeException {
+        Matrix A = new Matrix(4, 4);
+        Matrix B = new Matrix(A);
+        Assertions.assertEquals(B.getRows(), A.getRows());
+        Assertions.assertEquals(B.getColumns(), A.getColumns());
+    }
+
 }
